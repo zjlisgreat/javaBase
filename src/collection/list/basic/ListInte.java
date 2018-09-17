ef3bcc1396ec3c974054b8ceecced2576bf51c4f
zjlisgreat <2191669006@qq.com>
2018-09-18 00:16:46 +0800

    注释

--- src/collection/list/basic/ListInte.java
+++ src/collection/list/basic/ListInte.java
@@ -6,6 +6,9 @@ package collection.list.basic;
 public interface ListInte {
 	void test();
 	
+	/**
+	 * 接口中可以通过default关键字实现方法体
+	 */
 	default void test1() {
 		System.out.println("我是接口的方法test1");
 	}
