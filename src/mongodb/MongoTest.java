bd6e88807a80897c254826fa0a411465f336c761
zhujunli <2191669006@qq.com>
2019-07-26 16:46:39 +0800

    134

--- src/mongodb/MongoTest.java
+++ src/mongodb/MongoTest.java
@@ -12,19 +12,37 @@ import com.mongodb.client.MongoDatabase;
 public class MongoTest {
 	public static void main(String[] args) {
 		MongoClient client = new MongoClient("localhost", 27017);
-		//获取数据库
+		//获取数据库连接对象
 		MongoDatabase database =  client.getDatabase("test");
+		
 		//获取集合
 		MongoCollection<Document> collection = database.getCollection("test_db");
 		
-		//插入单个文档
+		//新增单个文档
 //		Document document = new Document("name","张三").append("sex", "男").append("age", 18);
 //		collection.insertOne(document);
-		List<Document> docs = new ArrayList<Document>();
-		for (int i = 0; i < 4; i++) {
-			Document doc = new Document("name","jack"+i).append("sex",i/2==0?"男":"女" ).append("age", 19+i);
-			docs.add(doc);
-		}
-		collection.insertMany(docs);
+		
+		//新增多个文档
+//		List<Document> docs = new ArrayList<Document>();
+//		for (int i = 0; i < 4; i++) {
+//			Document doc = new Document("name","rose"+i).append("sex",i/2==0?"女":"男" ).append("age", 18+i);
+//			docs.add(doc);
+//		}
+//		collection.insertMany(docs);
+		
+		//删除文档
+		//申明删除条件
+//		Bson bson = Filters.eq("age", 19);
+//		collection.deleteOne(bson);
+		
+		//删除多个文档 TODO
+		
+		//更新单个文档 TODO
+		
+		//更新多个文档 TODO
+		
+		//查询
+		
+		client.close();
 	}
 }
