ee1baaf7f6eb2891a5f3569eebe46a5ab5104b5c
zjlisgreat <zjlisgreat@JUNJUN>
2019-07-27 16:43:29 +0800

    java8sort

--- src/collection/stream/Java8Sort.java
+++ src/collection/stream/Java8Sort.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Java8Stream {
+public class Java8Sort {
 	public static void main(String[] args) {
 		List<String> names = new ArrayList<String>();
 		names.add("Google ");
