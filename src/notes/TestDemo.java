710c1f65f123143e0a142448b055bff8aa6eec6a
zhujunli <2191669006@qq.com>
2019-09-30 17:25:49 +0800

    使用continue跳出for循环异常，for循环是否继续执行

--- src/notes/TestDemo.java
+++ src/notes/TestDemo.java
@@ -2,9 +2,19 @@ package notes;
 /**
  *@author zjl
  *2018年9月18日 上午12:14:54
+ *使用continue跳出for循环异常，for循环是否继续执行
  */
 public class TestDemo {
-	public static void main(String[] args) {
-		System.out.println("测试使用!");
-	}
+	public static void main(String[] args){
+        for(int i=0;i<10;i++){
+          try{
+             System.out.println("i==========="+i);
+             int k=i/0;
+          }catch(Exception ex){
+              ex.printStackTrace();
+              System.out.println("ex======i====="+i +"===="+ex.getMessage());
+              continue;
+          }
+        }
+    }
 }
