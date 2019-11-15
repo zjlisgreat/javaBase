9de6552351e8db412901ce7c99dab7704beebcba
zhujunli <2191669006@qq.com>
2019-11-15 15:34:38 +0800

    json对象与map,实体类转化

---
+++ src/utils/json/JSONUtilDemo.java
@@ -0,0 +1,10 @@
+package utils.json;
+
+/**
+ * 这4个JSON类库分别为：Gson，FastJson，Jackson，Json-lib。
+ * @author zjl
+ *https://blog.csdn.net/zl1zl2zl3/article/details/85536515
+ */
+public class JSONUtilDemo {
+
+}
